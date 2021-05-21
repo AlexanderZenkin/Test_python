877a1889502c0d0cdd6d75b06b40177c597e8286
Alexander <aidik17@yandex.ru>
2021-05-21 08:38:04 +0300

    completed module 3

--- module_3/test_3-2-13.py
+++ module_3/test_3-2-13.py
@@ -1,12 +1,20 @@
 import unittest
+from selenium import webdriver
+import time
 
-
-class TestAbs(unittest.TestCase):
-    def test_abs1(self):
-        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
-
-    def test_abs2(self):
-        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+class test_page_registration(unittest.TestCase):
+    def test_init_driver(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        self.browser = webdriver.Chrome()
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector('div.first_block [class="form-control first"]').send_keys("Ivan")
+        self.browser.find_element_by_css_selector('div.first_block [class="form-control second"]').send_keys("Petrov")
+        self.browser.find_element_by_css_selector('div.first_block [class="form-control third"]').send_keys("IvanPetrov@mail.ru")
+        self.browser.find_element_by_css_selector("button.btn").click()
+        time.sleep(1)
+        welcome_text_elt = self.browser.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
 
 
 if __name__ == "__main__":
